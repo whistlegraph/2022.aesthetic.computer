0cd61a313fd4b6f95b6d80573cdb28f4cdeebe26
Jeffrey Alan Scudder <me@jas.life>
2022-01-05 19:10:09 -0800

    Refactor and smooth out socket support.

--- disks/public/spray.js
+++ disks/public/spray.js
@@ -1,17 +1,28 @@
 // Spray, 22.12.31
 // A stylus based painting tool for flower pictures.
 
-const sprays = [];
-let painting;
-let dot = false;
+let painting; // A bitmap to draw on.
+const sprays = []; // Points to draw.
+let dot = false; // Show preview dot while moving cursor.
+
+let server; // Networking
+const servers = {
+  me: "localhost:8082",
+  julias: "192.168.1.120:8082",
+  lucias: "192.168.1.245:8082",
+};
 
 // 🥾 Boot (Runs once before first paint and sim)
-function boot({ paste, cursor, painting: p, screen, resize }) {
-  // resize(screen.width * 3, screen.height * 3);
+function boot({ paste, cursor, painting: p, screen, net: { socket } }) {
   cursor("none");
   // Make & display the canvas.
-  painting = p(screen.width, screen.height, (gfx) => gfx.wipe(25, 60, 0));
+  painting = p(screen.width, screen.height, (gfx) => gfx.wipe(40, 50, 20));
   paste(painting);
+
+  // Connect to the server.
+  server = socket(servers.me, (type, content) => {
+    if (type === "point") sprays.push(content);
+  });
 }
 
 // 🎨 Paint (Runs once per display refresh rate)
@@ -28,30 +39,28 @@ function paint({
   if (sprays.length > 0) {
     // Spray on the painting within a circle.
     page(painting);
+
     sprays.forEach((s) => {
       const radius = 4;
       const sq = [-radius, radius];
       const cx = s.x;
       const cy = s.y;
 
-      function check() {
+      function randomInCircle() {
         const nx = cx + rnd(...sq);
         const ny = cy + rnd(...sq);
 
         if (dist(cx, cy, nx, ny) < radius) {
           return { x: nx, y: ny };
         } else {
-          return check();
+          return randomInCircle();
         }
       }
 
       let alpha = 255 * s.pressure * s.pressure;
-      // alpha *= alpha;
-      // map
 
       rep(8 + 32 * s.pressure, (i) => {
-        const point = check();
-
+        const point = randomInCircle();
         ink(
           60 + rnd(-40, 80), // R
           255 - rnd(0, 100), // G
@@ -63,11 +72,9 @@ function paint({
     sprays.length = 0;
     page(screen);
     paste(painting);
-    // Draw cursor.
-    ink(255, 0, 0).plot(pen.x, pen.y);
+    ink(255, 0, 0).plot(pen.x, pen.y); // Draw cursor.
   } else if (dot) {
-    // Hover cursor.
-    ink(255, 255, 0).plot(pen.x, pen.y);
+    ink(255, 255, 0).plot(pen.x, pen.y); // Hover cursor.
     dot = false;
   }
 }
@@ -75,7 +82,10 @@ function paint({
 // ✒ Act (Runs once per user interaction)
 function act({ event, plot }) {
   if (event.is("move")) dot = true;
-  if (event.is("draw") || event.is("touch")) sprays.push(event);
+  if (event.is("draw") || event.is("touch")) {
+    sprays.push(event);
+    server.send("point", { x: event.x, y: event.y, pressure: event.pressure });
+  }
 }
 
 // 💗 Beat (Runs once per bpm)
@@ -85,6 +95,5 @@ function act({ event, plot }) {
 // function sim($api) { // TODO: Move a ball here! }
 
 // 📚 Library (Useful functions used throughout the program)
-// ...
 
 export { boot, paint, act };
