1908183546455544da42d538f91fba5558b15fdf
Jeffrey Alan Scudder <me@jas.life>
2022-01-20 06:23:45 -0800

    Use more permanent socket url.

--- system/public/disks/common/servers.js
+++ system/public/disks/common/servers.js
@@ -1,5 +1,5 @@
 export const servers = {
-  do: "10.244.40.130:8082", // Digital Ocean deployment.
+  do: "wss://aesthetic-computer-xkldj.ondigitalocean.app:8082",
   me: "localhost:8082",
   julias: "192.168.1.120:8082",
   lucias: "192.168.1.245:8082",
