96d292b24fc6b5074bea9ff7607d397f50f0ff08
Jeffrey Alan Scudder <me@jas.life>
2022-01-20 06:33:56 -0800

    Adjust socket port.

--- system/public/disks/common/servers.js
+++ system/public/disks/common/servers.js
@@ -1,5 +1,5 @@
 export const servers = {
-  do: "wss://aesthetic-computer-xkldj.ondigitalocean.app:8082",
+  do: "aesthetic-computer-xkldj.ondigitalocean.app:443",
   me: "localhost:8082",
   julias: "192.168.1.120:8082",
   lucias: "192.168.1.245:8082",
