87d4b63c63c4e929555bdd7c60be996ac5405b74
Jeffrey Alan Scudder <me@jas.life>
2022-02-01 03:00:52 -0800

    Clean up sage demo code... add todos. Remove redundant conditional from spray.

--- system/public/disks/sage.js
+++ system/public/disks/sage.js
@@ -1,81 +1,69 @@
-// Sage, 2022.01.31.19.14
-// A basic demo.
-// TODO: Add `get, pix, pull, sample` (getPixel) to graph.js.
+// Sage, 2022.01.31.19.14 [Sage + Jeffrey]
+// A basic demo of a little walker.
 
-const { pow, floor, sin, cos, random, round } = Math;
+// TODO: Bake `wrap` and `pixel` into the api. 2022.02.01.02.46 (JAS)
+// TODO: Implement something via `sim` and be able to change the framerate.
+
+const { pow, floor, sin, cos, random } = Math;
 const { assign } = Object;
-const points = [];
-const location = { x: 0, y: 0 };
+
+let loc,
+  lastLoc = {};
 let theta = 0;
+const threshold = 100;
 
 // 🥾 Boot (Runs once before first paint and sim)
 function boot({ resize, fps, wipe, screen, cursor }) {
-  location.x = screen.width / 2;
-  location.y = screen.height / 2;
-  fps(1000);
-  //resize(64, 64);
-  cursor("none");
-  //wipe(255, 0, 0).ink(255).line(0, 0, screen.width, screen.height);
-  wipe(0, 0, 0);
-}
+  // fps(30); // Slow things down if you need to.
+  // resize(64, 64);
 
-// 🧮 Sim(ulate) (Runs once per logic frame (120fps locked)).
-function sim($api) {
-  //console.log($api);
+  cursor("none");
+  wipe(0, 0, 0);
+
+  loc = { x: screen.width / 2, y: screen.height / 2 };
 }
 
 // 🎨 Paint (Runs once per display refresh rate)
-function paint({ ink, line, plot, num, paintCount, screen }) {
-  function clampToScreen({ x, y }) {
-    x = (x / screen.width - floor(x / screen.width)) * screen.width;
-    y = (y / screen.height - floor(y / screen.height)) * screen.height;
-    return { x, y };
+function paint({ ink, line, paintCount, screen }) {
+  const selfColor = pixel(loc, screen)[0] / threshold + 1;
+
+  theta += random() - 0.5; // Choose a random angle.
+
+  // Rotate and move 🐢 by theta, scaled by underlying color.
+  loc.x += cos(theta) * selfColor;
+  loc.y += sin(theta) * selfColor;
+
+  loc = wrap(loc, screen.width, screen.height); // Wrap 🐢 to the screen.
+  assign(lastLoc, loc);
+
+  ink(25 + sin(0.01 * paintCount) * 128 + 128); // Oscillate color over time.
+
+  if (pow(lastLoc.x - loc.x, 2) + pow(lastLoc.y - loc.y, 2) < threshold) {
+    line(lastLoc.x, lastLoc.y, loc.x, loc.y);
   }
-
-  function getIndex({ x, y }) {
-    return (screen.width * floor(location.y) + floor(location.x)) * 4;
-  }
-
-  const selfColor = screen.pixels[getIndex(location)] / 29 + 1;
-
-  const lastLocation = { x: location.x, y: location.y };
-  assign(lastLocation, location);
-
-  theta += random() - 0.5;
-
-  location.x += cos(theta) * selfColor;
-  location.y += sin(theta) * selfColor;
-
-  const clamped = clampToScreen(location);
-  location.x = clamped.x;
-  location.y = clamped.y;
-
-  ink(25 + sin(0.01 * paintCount) * 128 + 128);
-
-  let threshold = 29;
-  if (
-    pow(lastLocation.x - location.x, 2) + pow(lastLocation.y - location.y, 2) <
-    threshold
-  )
-    line(lastLocation.x, lastLocation.y, location.x, location.y);
-  //}
 }
 
 // ✒ Act (Runs once per user interaction)
-function act({ event: e }) {
-  //if (e.is("draw")) points.push(e);
-}
+// function act({ event: e }) {
+// if (e.is("draw")) {}
+// }
 
-export { boot, sim, paint, act };
-
-/*
 // 💗 Beat (Runs once per bpm)
-function beat($api) {
-  // TODO: Play a sound here!
-}
- */
+// function beat($api) { /* Play a sound here! */ }
+
+// 🧮 Sim(ulate) (Runs once per logic frame (120fps locked)).
+// function sim($api) { /* Simulate something here! */ }
+
+export { boot, paint };
 
 // 📚 Library (Useful classes & functions used throughout the piece)
-// ...
+function wrap({ x, y }, w, h) {
+  x = (x / w - floor(x / w)) * w;
+  y = (y / h - floor(y / h)) * h;
+  return { x, y };
+}
 
-// export { boot, sim, paint, act, beat };
+function pixel({ x, y }, screen) {
+  const i = (screen.width * floor(y) + floor(x)) * 4;
+  return screen.pixels.subarray(i, i + 4);
+}
