024527749343ae410b3fe570a29f06b4d6ba3b3f
Jeffrey Alan Scudder <me@jas.life>
2022-06-27 23:00:47 -0400

    microphone cleanup

--- system/public/aesthetic.computer/disks/microphone.js
+++ system/public/aesthetic.computer/disks/microphone.js
@@ -1,37 +1,32 @@
 // Microphone, 2022.1.11.0.26
 // A simple audio + video feedback monitor test.
 // TODO: Add recording capability. 22.6.19.11.13
-// TODO: Fork off camera into a separate disk.
 
 const { floor } = Math;
 let mic;
 
 // 🎨 Paint (Runs once per display refresh rate)
-function paint({ wipe, ink, paste, screen: { width, height } }) {
+function paint({ wipe, ink, screen: { width, height } }) {
   mic?.poll(); // Query for updated amplitude and waveform samples from the mic.
-
-  wipe(15, 20, 0); // Clear the background.
+  wipe(15, 20, 0); // Clear background to green.
 
   if (mic?.amplitude) {
     const y = height - mic.amplitude * height;
-    ink(255, 16).line(0, y, width, y); // Draw line to represent overall volume.
+    ink(255, 16).line(0, y, width, y); // Horiz. line to represent total volume.
   }
 
   if (mic?.waveform) {
     const horizontalStep = width / mic.waveform.length + 1;
-    const yMid = height / 2;
-    const maxHeight = yMid;
-    let x = 0,
-      points = [];
-    // Calculate all the waveform points.
+    const yMid = height / 2,
+      maxHeight = yMid;
+    let points = [];
     mic.waveform.forEach((value, i) => {
-      points.push([x, yMid + value * maxHeight]);
-      x += horizontalStep;
+      points.push([i * horizontalStep, yMid + value * maxHeight]); // Get points.
     });
-    ink(255, 0, 0, 128).poly(points); // Draw the waveform.
+    ink(255, 0, 0, 128).poly(points); // Plot the waveform as a polyline.
   }
 
-  ink(0, 255, 0, 96).line(0, height / 2, width, height / 2); // Lime horizontal line.
+  ink(0, 255, 0, 96).line(0, height / 2, width, height / 2); // Horizontal lime.
 }
 
 // 💗 Beat (Runs once per bpm)
@@ -41,8 +36,5 @@ function beat({ sound: { beat, microphone } }) {
     mic = microphone;
   }
 }
-
-// 📚 Library (Useful functions used throughout the program)
-// ...
 
 export { paint, beat };
