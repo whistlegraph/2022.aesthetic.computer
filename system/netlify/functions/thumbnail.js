3f057d7b59538a5bcd50a55b6b8e8ad1c0b61f57
DIGITPAIN <me@jas.life>
2022-07-17 22:41:52 +0000

    optimize thumbnail size

--- system/netlify/functions/thumbnail.js
+++ system/netlify/functions/thumbnail.js
@@ -1,8 +1,5 @@
 const { builder } = require("@netlify/functions");
 
-// const chromium = require("chrome-aws-lambda");
-// const puppeteer = require("puppeteer-core");
-
 const playwright = require('playwright-aws-lambda');
 
 // Generates an image thumbnail of the starting screen of a piece.
@@ -28,9 +25,18 @@ async function handler(event, context) {
   const [width, height] = resolution.split("x").map((n) => parseInt(n));
 
   const chrome = await playwright.launchChromium();
-  console.log(chrome);
-  const browser = await chrome.newContext();
+  const browser = await chrome.newContext({
+    viewport: {
+      width: Math.ceil(width / 2),
+      height: Math.ceil(height / 2),
+    },
+    deviceScaleFactor: 2
+  });
   const page = await browser.newPage();
+
+  // TODO: Rewrite the URL below so that I can test locally without hitting
+  //       aesthetic.computer's production deployment. 22.07.17.22.30
+  //       - `https://${event.headers['x-forwarded-host']}/${command || ""}`
 
   await page.goto(`https://aesthetic.computer/${command.join("/") || ""}`, {
     waitUntil: "networkidle",
@@ -38,56 +44,13 @@ async function handler(event, context) {
 
   const buffer = await page.screenshot();
 
+  const buffer = await page.screenshot({
+    type: "jpeg",
+    quality: 80,
+  });
+
   await browser.close();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "image/png",
-      "Content-Length": buffer.length.toString()
-    },
-    body: buffer.toString("base64"),
-    ttl: 60,
-    isBase64Encoded: true,
-  };
-
-  /*
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: {
-      width: Math.ceil(width / 2),
-      height: Math.ceil(height / 2),
-      deviceScaleFactor: 2,
-    },
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-  });
-  */
-
-  // const page = await browser.newPage();
-
-  // TODO: Divide the screen density using a query parameter or zoom in using puppeteer?
-  // TODO: Rewrite the URL below so that I can test locally if custom thumbnails are needed.
-  // await page.goto(`https://${event.headers['x-forwarded-host']}/${command || ""}`); // Local? Codespaces?
-
-  //await page.goto(`https://aesthetic.computer/${command.join("/") || ""}`, {
-  //  waitUntil: "networkidle2",
-  //});
-
-  // Generally happens after first call to boot from a piece.
-  // So there is a built-in delay (to wait for a few frames to render).
-  //await page.waitForFunction("window.preloadReady === true", { timeout: 5000 });
-  //await page.waitForTimeout(1000); // TODO: Try to make this an explicit signal for
-  //                                        after something gets painted.
-
-  //const buffer = await page.screenshot({
-  //  type: "jpeg",
-  //  quality: 80,
-  //});
-
-  //await browser.close();
-
-  /*
   return {
     statusCode: 200,
     headers: {
@@ -98,7 +61,7 @@ async function handler(event, context) {
     ttl: 60,
     isBase64Encoded: true,
   };
-  */
+
 }
 
 exports.handler = builder(handler);
