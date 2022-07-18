49a245f4ecc8a920f9ecb7ddb52c11ddad387b5e
DIGITPAIN <me@jas.life>
2022-07-18 08:54:05 +0000

    add template into index function

--- system/netlify/functions/index.js
+++ system/netlify/functions/index.js
@@ -7,18 +7,43 @@
 
 const { builder } = require("@netlify/functions");
 const fs = require("fs").promises;
-const path = require("path");
 
 async function handler(event, context) {
-  let html = await fs.readFile(path.resolve("netlify/functions/templates/index.html"), "utf8");
+  //let html = require("./templates/index.html"); //await fs.readFile("./templates/index.html", "utf8");
 
-  let piece = event.path.slice(1);
-    
-  html = html.replace("1200x630", "1200x630/" + piece);
-  html = html.replace("800x800", "800x800/" + piece);
+  let html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>aesthetic.computer</title>
+        <script src="/aesthetic.computer/boot.js" type="module" defer></script>
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+        <link rel="stylesheet" href="/aesthetic.computer/style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="og:title" content="aesthetic.computer" />
+        <meta name="og:description" content="Run any piece by typing its name..." />
+        <meta name="og:image" content="https://aesthetic.computer/thumbnail/1200x630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="aesthetic.computer" />
+        <meta name="twitter:site" content="aesthetic.computer" />
+        <meta name="twitter:image" content="https://aesthetic.computer/thumbnail/800x800"/>
+      </head>
+      <body class="native-cursor"> <!-- Hides the 2D UI on first boot. -->
+      <script>
+        if (window.self !== window.top) document.body.classList.add("embed");
+      </script>
+      </body>
+    </html>
+  `;
 
-  if (piece.length) {
-    let title = piece + " - aesthetic.computer";
+  let path = event.path.slice(1);
+
+  html = html.replace("1200x630", "1200x630/" + path);
+  html = html.replace("800x800", "800x800/" + path);
+
+  if (path.length) {
+    let title = path + " - aesthetic.computer";
     html = html.replace(
       "<title>aesthetic.computer</title>",
       "<title>" + title + "</title>"
