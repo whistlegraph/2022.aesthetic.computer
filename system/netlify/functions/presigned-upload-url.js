0d16c299c7dd46a76b21dd87ab8e31d6ef055404
DIGITPAIN <me@jas.life>
2022-07-21 05:09:01 +0000

    presigner notes / planning

--- system/netlify/functions/presigned-upload-url.js
+++ system/netlify/functions/presigned-upload-url.js
@@ -1,12 +1,25 @@
-// Presigned Upload URL Generator
+// 🖋️ Presigned Upload URL Generator
 // This function generates a presigned URL for uploading to S3, and checks
 // the current bucket to make sure we are not uploading a duplicate file, by
 // autogenerating a uuid with nanoid.
 
+// 🔏 Security
+// Please ensure the bucket used to upload media has a CORS policy that
+// matches the hosting domain.
+
+// It should be... *.aesthetic.computer with
+// allowed methods GET, PUT and HEAD.
+
 // TODO
-// - [] Upload a file using a presigned url via the client.
+// - [] Upload the file using this presigned url, via the client.
 //  - Read: https://stackoverflow.com/a/28699269/8146077
+// - [] Show the media file experation date to the user.
+// - [] Visiting aesthetic.computer/art~code will show the
+//      file in a viewer.
 // - [x] Get presigned S3 URL from S3 bucket
+
+// Next version?
+// - [] Use a multi-part uploader.
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
