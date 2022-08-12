4f040f0436634221ef3870682a6317f9155b9cd4
Jeffrey Alan Scudder <me@jas.life>
2022-08-12 00:31:45 -0700

    add pink backdrop to `i` and up and down movement

--- system/public/aesthetic.computer/disks/i.mjs
+++ system/public/aesthetic.computer/disks/i.mjs
@@ -19,21 +19,31 @@ class I {
 const i = new I();
 
 // 🥾 Boot (Runs once before first paint and sim)
-function boot({ density }) {
-  density(8); // TODO: Should density be replaced with zoom or accept fractions?
+function boot({ density, resize }) {
+  //density(8); // TODO: Should density be replaced with zoom or accept fractions?
   // TODO: Runs only once!
-  // resize(50, 20);
+  resize(32, 32);
 }
 
 // 🎨 Paint (Executes every display frame)
 function paint({ wipe, ink, pan, unpan, line, point }) {
-  wipe(32); // Draw a gray background.
+  wipe(255, 100, 100); // Draw a gray background.
 
   // Draw the i character.
-  ink(200);
+  ink(255, 0, 0);
+
   pan(i.pos.x, i.pos.y);
   point(0, -2);
   line(0, 0, 0, i.len);
+
+  ink(255, 255, 0);
+  unpan();
+
+  pan(i.pos.x + 1, i.pos.y - 2);
+  point(0, -2);
+  line(0, 0, 0, i.len);
+
+
   unpan();
 
   return false; // Only once.
@@ -44,6 +54,11 @@ function sim({ needsPaint }) {
   if (i.dir.x !== 0) {
     i.pos.x += sign(i.dir.x) * i.vel.x;
     i.vel.x = min(i.vel.x * 1.1, 0.4); 
+  }
+
+  if (i.dir.y !== 0) {
+    i.pos.y += sign(i.dir.y) * i.vel.y;
+    i.vel.y = min(i.vel.y * 1.1, 0.4); 
   }
 
   // TODO: Replace with vec2 mag function?
@@ -63,8 +78,12 @@ function act({ event: e }) {
         i.vel.x = 0.1;
         break;
       case "ArrowUp":
+        i.dir.y = -1;
+        i.vel.y = 0.1;
         break;
       case "ArrowDown":
+        i.dir.y = 1;
+        i.vel.y = 0.1;
         break;
     }
   }
@@ -80,8 +99,12 @@ function act({ event: e }) {
         i.vel.x = 0;
         break;
       case "ArrowUp":
+        i.dir.y = 0;
+        i.vel.y = 0;
         break;
       case "ArrowDown":
+        i.dir.y = 0;
+        i.vel.y = 0;
         break;
     }
   }
