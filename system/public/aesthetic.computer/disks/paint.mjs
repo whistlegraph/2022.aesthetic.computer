b3f30704a0e777cb57226a55f3ad2cc321209613
Jeffrey Alan Scudder <me@jas.life>
2022-09-19 15:32:27 -0700

    todo

--- system/public/aesthetic.computer/disks/paint.mjs
+++ system/public/aesthetic.computer/disks/paint.mjs
@@ -1,6 +1,12 @@
 // Paint, 22.09.19.12.44 
 // Boilerplate for a distributed raster editor.
 
+// TODO: Turn this into a 'template' using...
+//       then boot, act, and leave can be removed...
+//       because they would be filled in!
+
+export const template = "paint";
+
 // 🥾 Boot (Runs once before first paint and sim)
 function boot({ screen, wipe, fps }) {
   if (!screen.load("painting")) wipe(64); // Load painting or wipe to gray. 
@@ -18,7 +24,6 @@ function act({ event: e }) {
     // TODO: Should be able to save a `.png` here...
   }
 }
-
 
 function leave({ store, screen }) {
   screen.save("painting");
