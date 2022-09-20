49baa260ff517d78f1c71b1f640f7d37bcb39293
Jeffrey Alan Scudder <me@jas.life>
2022-09-20 12:26:11 -0700

    hash disk.mjs on every load

--- system/public/aesthetic.computer/bios.mjs
+++ system/public/aesthetic.computer/bios.mjs
@@ -555,8 +555,8 @@ async function boot(parsed, bpm = 60, resolution, debug) {
   //const worker = new Worker("./aesthetic.computer/lib/disk.js", {
   //  type: "module",
   //});
-
-  const worker = new Worker(new URL("/aesthetic.computer/lib/disk.mjs", window.location.href), {
+  const fullPath = "/aesthetic.computer/lib/disk.mjs" + "#" + Date.now(); // bust the cache. This prevents an error related to Safari loading workers from memory.
+  const worker = new Worker(new URL(fullPath, window.location.href), {
     type: "module",
   });
 
--- system/public/aesthetic.computer/boot.mjs
+++ system/public/aesthetic.computer/boot.mjs
@@ -60,9 +60,7 @@ const bpm = 120; // Set the starting bpm. Is this still necessary?
 
 // TODO: Add params, search, and hash in here. 22.07.15.00.46
 
-setTimeout(function() {
-  boot(parsed, bpm, undefined, debug);
-}, 3000);
+boot(parsed, bpm, undefined, debug);
 
 // ***Incoming Message Responder***
 
