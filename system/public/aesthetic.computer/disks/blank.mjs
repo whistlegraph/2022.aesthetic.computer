07438f30f386a32b1e841647618d718925848ea0
Jeffrey Alan Scudder <me@jas.life>
2022-10-03 15:57:07 -0700

    new blank template

--- system/public/aesthetic.computer/disks/blank.mjs
+++ system/public/aesthetic.computer/disks/blank.mjs
@@ -1,32 +1,65 @@
-// Blank, 2021.11.28.03.04
+// Blank, 22.10.03.15.13 
+
+// Dear Piecemaker,
+
+// 🤸 Welcome to aesthetic.computer!
+
+// There aren't really any docs right now,
+// but you can `console.log($api);` or throw in
+// a `debugger` statement anywhere in a function
+// to explore what's available.
+
+// But what's even faster than that is to just ask 
+// me directly how to access and control the features
+// you're interested in using for a piece.
+
+// Run this piece by typing `@piecemaker/blank`
+// on aesthetic.computer or 4esthetic.com for short!
+
+// And debug in the dev console!
+
+// Jeffrey (me@jas.life / digitpain#2262 / @digitpain)
 
 // 🥾 Boot (Runs once before first paint and sim)
 function boot({ resize }) {
-  // TODO: Runs only once!
-  // resize(50, 20);
+  // Perform basic setup here.
+  // resize(50, 20); // Add a custom resolution. 
+}
+
+// 🎨 Paint (Executes every display frame)
+function paint($api) {
+  $api.wipe(255, 200, 200); // Draw a pink background
+  return false; // You can return false to draw only once!
+}
+
+// PS... I enjoy using `destructuring` to access the APIs!
+// This is the same as the above:
+// function paint({ wipe }) {
+//  wipe(255, 200, 200); // Draw a pink background
+//  return false; // You can return false to draw only once!
+// }
+
+// ✒ Act (Runs once per user interaction)
+function act({ event }) {
+  // Respond to user input here.
 }
 
 // 🧮 Sim(ulate) (Runs once per logic frame (120fps locked)).
 function sim($api) {
-  // TODO: Move a ball here!
-  //console.log($api);
+  // Crunch numbers outside of rendering here.
 }
-
-// 🎨 Paint (Executes every display frame)
-function paint({ wipe }) {
-  wipe(128); // Draw a gray background
-  return false; // Only once.
-}
-
-// ✒ Act (Runs once per user interaction)
-function act({ event }) {}
 
 // 💗 Beat (Runs once per bpm, starting when the audio engine is activated.)
 function beat($api) {
-  // TODO: Play a sound here!
+  // Make sound here.
+}
+
+// 👋 Leave (Runs once before the piece is unloaded)
+function leave($api) {
+  // Pass data to the next piece here.
 }
 
 // 📚 Library (Useful functions used throughout the piece)
 // ...
 
-export { boot, sim, paint, act, beat };
+export { boot, sim, paint, act, beat, leave };
